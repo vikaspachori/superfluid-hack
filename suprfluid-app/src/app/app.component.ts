be982036e103783e5e2f0978df178158a6166e38
vikaspachori <vikas_pachori@yahoo.com>
2021-07-23 20:49:45 +0530

    first commit

---
+++ suprfluid-app/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+import Web3 from "web3";
+import { SuperfluidSDK } from "@superfluid-finance/js-sdk"
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'suprfluid-App';
+}
